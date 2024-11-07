1e5f3ef23f9192888fcbea0c38818403ede359ce
Diego Juan Isaquiel <60893500+DiegoJuanIsaquiel@users.noreply.github.com>
2024-11-07 19:49:56 -0300

    Initialize Spring Project

---
+++ src/main/java/br/facens/ac2/DevOpsQaApplication.java
@@ -0,0 +1,13 @@
+package br.facens.ac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsQaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/facens/ac2/DevOpsQaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.facens.ac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
